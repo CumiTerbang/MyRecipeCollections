0403a6981a255b20721753e0e200fbaf9326dbde
CumiTerbang <gitarringsek@gmail.com>
2021-07-27 12:07:56 +0700

    update Title Screen Page

---
+++ lib/about_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About"),
+      ),
+      body: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+          child: Column(
+            children: [
+              Text("My Recipe Collection",
+                  style:
+                      TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+              Divider(height: 20),
+              Text(
+                  "Welcome to my simple flutter project.\nIn this project I try to create simple mobile apps for practice flutter to browse various food recipe using Food2Fork API",
+                  style:
+                      TextStyle(fontSize: 14.0)),
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/titlescreen_page.dart
@@ -0,0 +1,77 @@
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:my_recipe_collections/about_page.dart';
+
+class TitleScreenPage extends StatelessWidget {
+  Container imageBox(String imgsrc) {
+    return Container(
+        width: 150.0,
+        height: 150.0,
+        decoration: new BoxDecoration(
+            shape: BoxShape.rectangle,
+            image: new DecorationImage(
+                fit: BoxFit.fill, image: new AssetImage(imgsrc))));
+  }
+
+  ConstrainedBox buildButtonNextPage(
+      BuildContext context, String label, Widget targetPage) {
+    return ConstrainedBox(
+      constraints: BoxConstraints.tightFor(width: 200),
+      child: ElevatedButton(
+          child: Text(label),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => targetPage),
+            );
+          }),
+    );
+  }
+
+  Widget buildWhiteScreen() {
+    return Column(
+      children: [
+        Expanded(
+          child: Container(color: Color(0x90FFFFFF)),
+        ),
+      ],
+    );
+  }
+
+  Widget buildBodyContent(BuildContext context) {
+    return Stack(children: [
+      Positioned.fill(
+        //
+        child: Image(
+          image: AssetImage('assets/background.jpg'),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+      buildWhiteScreen(),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          imageBox("assets/my_logo.png"),
+          Divider(
+            height: 30.0,
+          ),
+          buildButtonNextPage(context, "Get Started", AboutPage()),
+          buildButtonNextPage(context, "About", AboutPage()),
+        ],
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DoubleBackToCloseApp(
+        snackBar: const SnackBar(
+          content: Text('Tap back again to leave'),
+        ),
+        child: buildBodyContent(context),
+      ),
+    );
+  }
+}
